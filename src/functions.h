10001cdc7be395b30f2ff74913f1fb8a2b7c20ec
Anexius54 <e.sapronov@yandex.ru>
2019-06-12 22:12:40 +0700

    Add program

---
+++ src/functions.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stddef.h>
+
+#define SIZE 255
+
+typedef struct Words {
+  char **word;
+  char buffer[50][SIZE];
+  size_t qWords;
+} Words;
+
+void getDataFromStdin(Words *w);
+void getDataFromFile(Words *w);
+void output(Words *w);
+void sorting(Words *w);
+
+void format(char *buf);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include "functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  Words *w = (Words *)malloc(sizeof(*w));
+  if (!w) {
+    printf("Allocation error\n");
+    exit(1);
+  }
+
+  w->qWords = 10;
+  getDataFromFile(w);
+
+  sorting(w);
+  output(w);
+  free(w);
+
+  return 0;
+}
